3826c77c67a1d303085f5421c2bcb2372ff53dac
Giantory <giantory.bryant@gmail.com>
2022-12-15 23:17:55 -0500

    add excel uploader

--- src/entities/drivers/drivers.routes.js
+++ src/entities/drivers/drivers.routes.js
@@ -1,9 +1,27 @@
 import { Router } from 'express';
+import multer from 'multer';
+import readXlsxFile from 'read-excel-file/node'
+
+
 
 //controllers
 import getAllDrivers from './useCases/getAllDrivers.js';
 import getActiveDrivers from './useCases/getActiveDrivers.js';
 import getDriver from './useCases/getDriver.js';
+import postDrivers from './useCases/postDriversFromExcel.js';
+import path from 'path';
+
+const storage = multer.diskStorage({
+    destination: "./public/uploads/files",
+    filename: function (req, file, cb) {
+      cb(
+        null,
+        file.originalname + "-" + Date.now() + path.extname(file.originalname)
+      );
+    },
+  });
+  const upload = multer({ storage: storage });
+
 
 const router = new Router();
 
@@ -12,6 +30,7 @@ const router = new Router();
 router.post(`/api/drivers/getAllDrivers`, getAllDrivers);
 router.post(`/api/drivers/getActiveDrivers`, getActiveDrivers);
 router.post(`/api/drivers/getDriver`, getDriver);
+router.post(`/api/drivers/postDriver`,  upload.single("file"), postDrivers);
 
 
 export default router
--- src/entities/drivers/useCases/postDriversFromExcel.js
+++ src/entities/drivers/useCases/postDriversFromExcel.js
@@ -1,18 +1,38 @@
 import { mysqlConnection } from '../../../config/database.js';
+import multer from 'multer';
+import readXlsxFile from 'read-excel-file/node'
+import path from 'path'
+import fs from 'fs';
+import { nanoid } from 'nanoid'
 
 
-const postDriver = (req, res) => {
-
-   
-    mysqlConnection.query(
-        ` SELECT UPPER(CONCAT(nameuser,' ' , lastname)) fullname, document ,company, credit, placa_car, license, phone_contact FROM users_drivers WHERE id_firebase = ${req.body.driver}`
-        , (err, rows, fields) => {
-            if (!err) {
-                res.json(rows)
-            } else {
-                console.log(err)
-            }
-        })
+const postDrivers = (req, res) => {
+    try {
+        importFileToDb(req.file.path, req.body.district, req.body.province, req.body.department);
+        return res.status(200).json({ message: 'Datos importados correctamente', status: 'OK' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: 'Error al importar los datos', status: 'ERROR' })
+    }
 }
 
-export default postDriver
+function importFileToDb(exFile, district, province, department) {
+    readXlsxFile(exFile).then((rows) => {
+        const data = rows.shift()
+        for (let i = 0; i < rows.length; i++) {
+            const id = nanoid();
+            rows[i].unshift(id);
+            rows[i].push(department);
+            rows[i].push(province);
+            rows[i].push(district);
+        }
+        console.log([rows]);
+        let query = 'INSERT INTO users_drivers (id_firebase, company, document,' +
+            'document_type, nameuser, lastname, model_car, license, phone_contact, placa_car, department_id, province_id, district_id) VALUES ?'
+        mysqlConnection.query(query, [rows], (error, response) => {
+            console.log(error || response)
+        })
+    })
+}
+
+export default postDrivers
