a4ab071dd1aede742c789a198514b3856ec8ceb0
Giantory <giantory.bryant@gmail.com>
2022-12-29 18:24:08 -0500

    add getAllPayments endpoint

--- api/entities/payments/payments.routes.js
+++ api/entities/payments/payments.routes.js
@@ -2,15 +2,16 @@ import { Router } from 'express';
 
 //controllers
 import getPaymentsPerDriver from './useCases/getPaymentsPerDriver.js';
+import getAllPayments from './useCases/getAllPayments.js';
 
 
 
 const router = new Router();
 
 router.post(`/api/payments/getPaymentsPerDriver`, getPaymentsPerDriver);
+router.post(`/api/payments/getAllPayments`, getAllPayments);
 
 
 export default router;
-
 
 
--- api/entities/payments/useCases/getAllPayments.js
+++ api/entities/payments/useCases/getAllPayments.js
@@ -2,10 +2,49 @@ import { mysqlConnection } from '../../../config/database.js';
 
 
 const getAllPayments = (req, res) => {
+    const departmentFilterConditional = () => {
+        if (req.body.department && req.body.department !== 0) {
+            return ` AND ud.department_id=${req.body.department}`;
+        } else {
+            return '';
+        }
+        return "";
+    }
+    const provinceFilterConditional = () => {
+        if (req.body.province && req.body.province !== 0) {
+            return ` AND ud.province_id=${req.body.province}`;
+        } else {
+            return '';
+        }
+        return "";
+    }    
+    
+    const districtFilterConditional = () => {
+        if (req.body.district && req.body.district !== 0) {
+            return ` AND ud.district_id=${req.body.district}`;
+        } else {
+            return '';
+        }
+        return "";
+    }
+
+    const createdFilterConditional = () => {
+        if (req.body.created && req.body.created !== 0) {
+            return ` AND DATEDIFF(NOW(), create_at) <${req.body.created}`;
+        } else {
+            '';
+        }
+        return "";
+    }
 
    
     mysqlConnection.query(
-        ` SELECT id, name FROM provincies WHERE department_id = ${req.body.department}`
+        ` SELECT COUNT(*) AS COUNT, MONTHNAME(DATE(mp.create_at)) AS DATE`+ 
+        ` FROM mercadopago_payments mp`+
+        ` JOIN users_drivers ud`+
+        ` ON mp.external_reference = ud.id_firebase`+
+        ` WHERE mp.create_at >= DATE_SUB(CURDATE(), INTERVAL 12 MONTH) ${createdFilterConditional()}${provinceFilterConditional()}${departmentFilterConditional()}${districtFilterConditional()}`+
+        ` GROUP BY date `
         , (err, rows, fields) => {
             if (!err) {
                 res.json(rows)
